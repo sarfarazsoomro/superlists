35b392be5cf0f2ad6ecf3f122144673c25e264fe
sarfarazsoomro <sarfarazsoomro@yahoo.com>
2016-08-06 22:00:52 -0700

    Update home template and functional tests

--- 01_functional_tests.py
+++ 01_functional_tests.py
@@ -24,8 +24,8 @@ class NewVisitorTest(unittest.TestCase):
     # She is invited to enter a to-do item straight away
     inputbox = self.browser.find_element_by_id('id_new_item')
     self.assertEqual(
-      inputbox.get_attribute('placeholder'),
-      'Enter a to-do item'
+      'Enter a to-do item',
+      inputbox.get_attribute('placeholder')
     )
 
     # She types "Buy peacock feathers" into a text box (Edith's hobby
@@ -39,7 +39,8 @@ class NewVisitorTest(unittest.TestCase):
     table = self.browser.find_element_by_id('id_list_table')
     rows = table.find_elements_by_tag_name('tr')
     self.assertTrue(
-      any(row.text == '1: Buy peacock feathers' for row in rows)
+      any(row.text == '1: Buy peacock feathers' for row in rows),
+      'New to-do item did not appear in the table'
     )
 
     # There is still a text box inviting her to add another item. She
--- lists/tests.py
+++ lists/tests.py
@@ -17,4 +17,3 @@ class HomePageTest(TestCase):
 
     expected_html = render_to_string('home.html')
     self.assertEqual(response.content.decode(), expected_html)
-    
