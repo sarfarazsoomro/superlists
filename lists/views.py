e147903d8032711461246fb467f734f37600a995
sarfarazsoomro <sarfarazsoomro@yahoo.com>
2016-08-06 17:04:37 -0700

    Added tests for home page

--- lists/views.py
+++ lists/views.py
@@ -1,4 +1,8 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
-def home_page():
-  pass
+def home_page(request):
+  responseString = """<html>
+    <title>To-Do lists</title>
+  </html>"""
+  return HttpResponse(responseString)
