3e33731063eaa6dc53105e2d482c1cd91a765827
Sarfaraz Soomro <sarfarazs@wepay.com>
2016-09-25 00:24:51 -0700

    Uncomment get_latest_source in fab file

--- deploy_tools/fabfile.py
+++ deploy_tools/fabfile.py
@@ -8,7 +8,7 @@ def deploy():
   site_folder = '/home/%s/sites/%s' % (env.user, env.host)
   source_folder = site_folder + '/source'
   _create_directory_structure_if_necessary(site_folder)
-  # _get_latest_source(source_folder)
+  _get_latest_source(source_folder)
   _update_settings(source_folder, env.host)
   _update_virtualenv(source_folder)
   _update_static_files(source_folder)
